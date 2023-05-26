d81de7b044a5df158b099d599f9ea11a959c85a2
Kalixtan <97067578+Kalixtan@users.noreply.github.com>
2023-05-26 15:51:01 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'gameEngine3 - \'Unofficial\''
+copyright = '2023, NoxyMe'
+author = 'NoxyMe'
 
 release = '0.1'
 version = '0.1.0'
@@ -20,7 +20,6 @@ extensions = [
 ]
 
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
 intersphinx_disabled_domains = ['std']
